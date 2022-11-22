cc3b5be6acef0d9b4c8bf6f20409c9fc4cbb2aa0
MahaShahwar <mahishahwar2@gmail.com>
2022-11-22 10:55:12 +0500

    make it better

--- src/routes/web.js
+++ src/routes/web.js
@@ -8,12 +8,6 @@ let initWebRoutes = (app)=> {
     router.get("/", chatfbController.getHomepage);
     router.post("/webhook",chatfbController.postWebhook)
     router.get("/webhook", chatfbController.getWebhook);
-    //router.post("/webhook", homepageController.postWebhook);
-    // router.post("/set-up-profile", homepageController.handleSetupProfile);
-    // router.get("/set-up-profile", homepageController.getSetupProfilePage);
-
-    // router.get("/info-order", homepageController.getInfoOrderPage);
-    // router.post("/set-info-order", homepageController.setInfoOrder);
     return app.use("/", router);
 };
 
