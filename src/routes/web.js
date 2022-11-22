5ae2aaf5dbeb57600008431ef2cb2edb9c06bf56
MahaShahwar <mahishahwar2@gmail.com>
2022-11-22 10:38:24 +0500

    make it better

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,12 +1,11 @@
 import express from "express";
-import homepageController from "../controllers/homepageController";
 import chatfbController from "../controllers/chatfbController";
 
 
 let router = express.Router();
 
 let initWebRoutes = (app)=> {
-    router.get("/", homepageController.getHomePage);
+    //router.get("/", homepageController.getHomePage);
     router.post("/webhook",chatfbController.postWebhook)
     router.get("/webhook", chatfbController.getWebhook);
     //router.post("/webhook", homepageController.postWebhook);
